a03464452a62cdff145c9c8ed899df74935edd25
Dalibor Pavicic <dalibor-pavicic@hotmail.com>
2016-08-19 16:26:34 +0200

    Add logic for displaying corresponding fragment when drawer menu item is clicked

--- app/src/main/java/rs/ac/uns/ftn/chiefcook/ui/activities/BaseActivity.java
+++ app/src/main/java/rs/ac/uns/ftn/chiefcook/ui/activities/BaseActivity.java
@@ -1,14 +1,18 @@
 package rs.ac.uns.ftn.chiefcook.ui.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 import rs.ac.uns.ftn.chiefcook.R;
 
-public class BaseActivity extends AppCompatActivity {
+public class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    public static final String DRAWER_ITEM_ID_KEY = "drawer_item_id_key";
 
     protected DrawerLayout drawerLayout;
     protected NavigationView navigationView;
@@ -22,5 +26,37 @@ public class BaseActivity extends AppCompatActivity {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         frameLayout = (FrameLayout) findViewById(R.id.content_frame);
+
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(this);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        item.setChecked(true);
+
+        switch (item.getItemId()) {
+            case R.id.home:
+                displayActivity(FragmentHolderActivity.class, 0);
+                return true;
+            case R.id.recipes:
+                displayActivity(FragmentHolderActivity.class, 1);
+                return true;
+            case R.id.favorite_recipes:
+                displayActivity(FragmentHolderActivity.class, 2);
+                return true;
+            default:
+                return true;
+        }
+    }
+
+    private void displayActivity(Class activityClass, int drawerItemIndex) {
+        drawerLayout.closeDrawers();
+        Intent intent = new Intent(this, activityClass);
+        intent.putExtra(DRAWER_ITEM_ID_KEY, drawerItemIndex);
+        startActivity(intent);
+        this.overridePendingTransition(0, 0); // disable animation when starting the activity
+        finish(); // finish the current activity
     }
 }
--- app/src/main/java/rs/ac/uns/ftn/chiefcook/ui/activities/FragmentHolderActivity.java
+++ app/src/main/java/rs/ac/uns/ftn/chiefcook/ui/activities/FragmentHolderActivity.java
@@ -1,8 +1,14 @@
 package rs.ac.uns.ftn.chiefcook.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 
 import rs.ac.uns.ftn.chiefcook.R;
+import rs.ac.uns.ftn.chiefcook.ui.fragments.FavoriteRecipesFragment;
+import rs.ac.uns.ftn.chiefcook.ui.fragments.HomeFragment;
+import rs.ac.uns.ftn.chiefcook.ui.fragments.RecipesFragment;
 
 public class FragmentHolderActivity extends BaseActivity {
 
@@ -11,5 +17,25 @@ public class FragmentHolderActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
 
         getLayoutInflater().inflate(R.layout.activity_fragment_holder, frameLayout);
+        Intent intent = getIntent();
+        int fragmentIndex = intent.getIntExtra(DRAWER_ITEM_ID_KEY, 0);
+
+        Fragment fragment;
+
+        switch (fragmentIndex) {
+            case 1:
+                fragment = new RecipesFragment();
+                break;
+            case 2:
+                fragment = new FavoriteRecipesFragment();
+                break;
+            default:
+                fragment = new HomeFragment();
+        }
+
+        FragmentTransaction fragmentTransaction =
+                getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 }
