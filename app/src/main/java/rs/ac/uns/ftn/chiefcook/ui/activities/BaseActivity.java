1bf14c5abc09f4cc084a22bb2096f74ed9fe3e73
Dalibor Pavicic <dalibor-pavicic@hotmail.com>
2016-08-19 22:01:57 +0200

    Open navigation drawer on toolbar menu button click

--- app/src/main/java/rs/ac/uns/ftn/chiefcook/ui/activities/BaseActivity.java
+++ app/src/main/java/rs/ac/uns/ftn/chiefcook/ui/activities/BaseActivity.java
@@ -3,8 +3,10 @@ package rs.ac.uns.ftn.chiefcook.ui.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
@@ -34,6 +36,23 @@ public class BaseActivity extends AppCompatActivity implements NavigationView.On
         if (navigationView != null) {
             navigationView.setNavigationItemSelectedListener(this);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_base, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
