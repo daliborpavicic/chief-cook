49ec51f931c5056dc3f0d66acb2ba2a00157ff8b
Dalibor Pavicic <dalibor-pavicic@hotmail.com>
2016-08-20 16:22:21 +0200

    Inflate home fragment menu and attach query text listener on the search view

--- app/src/main/java/rs/ac/uns/ftn/chiefcook/ui/fragments/HomeFragment.java
+++ app/src/main/java/rs/ac/uns/ftn/chiefcook/ui/fragments/HomeFragment.java
@@ -3,9 +3,13 @@ package rs.ac.uns.ftn.chiefcook.ui.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -14,8 +18,10 @@ import rs.ac.uns.ftn.chiefcook.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements SearchView.OnQueryTextListener {
 
+
+    public static final String LOG_TAG = HomeFragment.class.getSimpleName();
 
     public HomeFragment() {
         // Required empty public constructor
@@ -25,15 +31,39 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
 
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
         getActivity().setTitle(R.string.title_home);
 
-        return  rootView;
+        return rootView;
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        menu.findItem(R.id.action_search).setVisible(true);
+        super.onPrepareOptionsMenu(menu);
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menu_home_fragment, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(this);
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        Log.d(LOG_TAG, query);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
     }
 }
