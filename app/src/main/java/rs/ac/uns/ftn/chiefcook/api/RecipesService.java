b699ce6f59d87e72ef7148ded219f20524986c92
Dalibor Pavicic <dalibor-pavicic@hotmail.com>
2016-08-21 11:28:44 +0200

    Create recipe service interface and declare method for searching recipes

---
+++ app/src/main/java/rs/ac/uns/ftn/chiefcook/api/RecipesService.java
@@ -0,0 +1,23 @@
+package rs.ac.uns.ftn.chiefcook.api;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import rs.ac.uns.ftn.chiefcook.model.RecipesListResponse;
+
+/**
+ * Created by daliborp on 20.8.16..
+ */
+public interface RecipesService {
+
+    @GET("/recipes/search")
+    public Call<RecipesListResponse> searchRecipes(@Query("query") String query,
+                                                   @Query("number") Integer number,
+                                                   @Query("type") String type,
+                                                   @Query("cuisine") String cuisine,
+                                                   @Query("diet") String diet,
+                                                   @Query("excludeIngredients") String excludeIngredients,
+                                                   @Query("intolerances") String intolerances,
+                                                   @Query("limitLicense") Boolean limitLicense,
+                                                   @Query("offset") Integer offset);
+}
