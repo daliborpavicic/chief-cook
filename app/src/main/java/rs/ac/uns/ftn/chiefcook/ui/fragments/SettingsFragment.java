18f61b6f8e5355c77de2d32dff5cb076d61fd0c4
Dalibor Pavicic <dalibor-pavicic@hotmail.com>
2016-08-26 16:47:33 +0200

    Create settings fragment and xml for defining preferences

---
+++ app/src/main/java/rs/ac/uns/ftn/chiefcook/ui/fragments/SettingsFragment.java
@@ -0,0 +1,17 @@
+package rs.ac.uns.ftn.chiefcook.ui.fragments;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+import rs.ac.uns.ftn.chiefcook.R;
+
+/**
+ * Created by daliborp on 26.8.16..
+ */
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle bundle, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+}
