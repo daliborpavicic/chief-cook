5e9b21795540822f1db2025c67b2147ef297c8b1
Dalibor Pavicic <dalibor-pavicic@hotmail.com>
2016-08-26 15:51:44 +0200

    Create splash activity with animated text and show it on an application startup

---
+++ app/src/main/java/rs/ac/uns/ftn/chiefcook/ui/activities/SplashActivity.java
@@ -0,0 +1,50 @@
+package rs.ac.uns.ftn.chiefcook.ui.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import rs.ac.uns.ftn.chiefcook.R;
+
+/**
+ * Created by daliborp on 26.8.16..
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final TextView splashText = (TextView) findViewById(R.id.tvSplashText);
+        final Animation rotation = AnimationUtils.loadAnimation(this, R.anim.rotate);
+
+        final Animation fadeAnimation = AnimationUtils.loadAnimation(this,
+                android.support.v7.appcompat.R.anim.abc_fade_out);
+
+        rotation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                splashText.startAnimation(fadeAnimation);
+                finish();
+                Intent intent = new Intent(SplashActivity.this, FragmentHolderActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        splashText.startAnimation(rotation);
+    }
+}
