25aea68805816bbe9c530ad1792b798f491d3b87
Dalibor Pavicic <dalibor-pavicic@hotmail.com>
2016-08-26 21:09:08 +0200

    Refactor splash screen to show it only if there is a real need in the application in according to Google's recommendation

--- app/src/main/java/rs/ac/uns/ftn/chiefcook/ui/activities/SplashActivity.java
+++ app/src/main/java/rs/ac/uns/ftn/chiefcook/ui/activities/SplashActivity.java
@@ -2,13 +2,7 @@ package rs.ac.uns.ftn.chiefcook.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.TextView;
-
-import rs.ac.uns.ftn.chiefcook.R;
 
 /**
  * Created by daliborp on 26.8.16..
@@ -18,36 +12,9 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
-        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
-
-        final TextView splashText = (TextView) findViewById(R.id.tvSplashText);
-        final Animation rotation = AnimationUtils.loadAnimation(this, R.anim.rotate);
-
-        final Animation fadeAnimation = AnimationUtils.loadAnimation(this,
-                android.support.v7.appcompat.R.anim.abc_fade_out);
-
-        rotation.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                splashText.startAnimation(fadeAnimation);
-                finish();
-                Intent intent = new Intent(SplashActivity.this, FragmentHolderActivity.class);
-                startActivity(intent);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
-
-        splashText.startAnimation(rotation);
+        Intent intent = new Intent(this, FragmentHolderActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
---
+++ app/src/main/java/rs/ac/uns/ftn/chiefcook/util/Utils.java
@@ -0,0 +1,8 @@
+package rs.ac.uns.ftn.chiefcook.util;
+
+/**
+ * Created by daliborp on 26.8.16..
+ */
+public class Utils {
+
+}
