5e858beae41074bae62aef58c14ddb1c0afab888
Dalibor Pavicic <dalibor-pavicic@hotmail.com>
2016-08-31 22:39:56 +0200

    Load recipe instructions and show them in recipe details activity

--- app/src/main/java/rs/ac/uns/ftn/chiefcook/ui/activities/RecipeDetailsActivity.java
+++ app/src/main/java/rs/ac/uns/ftn/chiefcook/ui/activities/RecipeDetailsActivity.java
@@ -5,14 +5,20 @@ import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,20 +29,26 @@ import rs.ac.uns.ftn.chiefcook.R;
 import rs.ac.uns.ftn.chiefcook.api.RecipesService;
 import rs.ac.uns.ftn.chiefcook.api.SpoonacularApi;
 import rs.ac.uns.ftn.chiefcook.model.Recipe;
+import rs.ac.uns.ftn.chiefcook.model.RecipeInstructionsItem;
+import rs.ac.uns.ftn.chiefcook.model.Step;
+import rs.ac.uns.ftn.chiefcook.ui.adapters.RecipeStepAdapter;
 
 public class RecipeDetailsActivity extends AppCompatActivity {
 
+    public static final String LOG_TAG = RecipeDetailsActivity.class.getSimpleName();
     public static final String RECIPE_ID_KEY = "recipe_key";
 
     @BindView(R.id.toolbar) protected Toolbar toolbar;
     @BindView(R.id.collapsing_toolbar) protected CollapsingToolbarLayout collapsingToolbar;
     @BindView(R.id.ivRecipeImage) protected ImageView tvRecipeImage;
+    @BindView(R.id.rvRecipeSteps) protected RecyclerView rvRecipeSteps;
 
-    private RecipesService recipesService;
     private ShareActionProvider shareActionProvider;
 
+    private RecipesService recipesService;
+    private RecipeStepAdapter recipeStepAdapter;
+    private List<Step> recipeSteps;
     private String recipeName;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +64,18 @@ public class RecipeDetailsActivity extends AppCompatActivity {
         final int recipeId = intent.getIntExtra(RECIPE_ID_KEY, 0);
 
         recipesService = SpoonacularApi.getRecipesService();
+        recipeSteps = new ArrayList<>();
+        recipeStepAdapter = new RecipeStepAdapter(this, recipeSteps);
+
+        rvRecipeSteps.setAdapter(recipeStepAdapter);
+
+        LinearLayoutManager linearLayoutManager =
+                new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        rvRecipeSteps.setLayoutManager(linearLayoutManager);
+
         loadRecipe(recipeId);
+        loadRecipeInstructions(recipeId);
     }
 
     private void loadRecipe(int recipeId) {
@@ -80,6 +103,33 @@ public class RecipeDetailsActivity extends AppCompatActivity {
                         Toast.LENGTH_SHORT);
 
                 failureToast.show();
+            }
+        });
+    }
+
+    private void loadRecipeInstructions(final int recipeId) {
+        Call<List<RecipeInstructionsItem>> call = recipesService.getAnalyzedRecipeInstructions(recipeId);
+        call.enqueue(new Callback<List<RecipeInstructionsItem>>() {
+            @Override
+            public void onResponse(Call<List<RecipeInstructionsItem>> call, Response<List<RecipeInstructionsItem>> response) {
+                List<RecipeInstructionsItem> recipeInstructionsItems = response.body();
+                if (recipeInstructionsItems.isEmpty()) {
+                    Log.d(LOG_TAG, String.format("Empty instructions list for recipe id= %d", recipeId));
+                } else {
+                    RecipeInstructionsItem recipeInstructionsItem = recipeInstructionsItems.get(0);
+                    int currentItemCount = recipeStepAdapter.getItemCount();
+                    recipeSteps.addAll(recipeInstructionsItem.getSteps());
+
+                    recipeStepAdapter.notifyItemRangeInserted(currentItemCount,
+                            recipeSteps.size() - 1);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<RecipeInstructionsItem>> call, Throwable t) {
+                Toast toast = Toast.makeText(RecipeDetailsActivity.this,
+                        t.getLocalizedMessage(), Toast.LENGTH_SHORT);
+                toast.show();
             }
         });
     }
