fda319f4ab15ac8826d7a3e576a3a6c6362bf490
Dalibor Pavicic <dalibor-pavicic@hotmail.com>
2016-09-03 19:06:52 +0200

    Create the contract and SQL helper class for application database

---
+++ app/src/main/java/rs/ac/uns/ftn/chiefcook/data/ChiefCookContract.java
@@ -0,0 +1,67 @@
+package rs.ac.uns.ftn.chiefcook.data;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by daliborp on 3.9.16..
+ */
+public final class ChiefCookContract {
+
+    public static final String CONTENT_AUTHORITY = "rs.ac.uns.ftn.chiefcook.data";
+
+    private static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_RECIPE = "recipe";
+    public static final String PATH_INGREDIENT = "ingredient";
+
+    public static final class RecipeEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_RECIPE).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_RECIPE;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_RECIPE;
+
+        public static final String TABLE_NAME = "recipe";
+
+        public static final String COLUMN_TITLE = "title";
+        public static final String COLUMN_READY_IN_MINUTES = "readyInMinutes";
+        public static final String COLUMN_IMAGE_URL = "recipeImageUrl";
+        public static final String COLUMN_API_ID = "recipeApiId";
+
+        public static Uri buildRecipeUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+
+    public static final class IngredientEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_INGREDIENT).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_INGREDIENT;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_INGREDIENT;
+
+        public static final String TABLE_NAME = "ingredient";
+
+        public static final String COLUMN_NAME= "name";
+        public static final String COLUMN_AMOUNT = "amount";
+        public static final String COLUMN_UNIT = "unit";
+        public static final String COLUMN_UNIT_SHORT = "unitShort";
+        public static final String COLUMN_IMAGE_URL = "ingredientImageUrl";
+        public static final String COLUMN_ORIGINAL_STRING = "originalString";
+        public static final String COLUMN_API_ID = "ingredientApiId";
+        public static final String COLUMN_RECIPE_ID = "recipeId";
+
+        public static Uri buildIngredientnUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+}
---
+++ app/src/main/java/rs/ac/uns/ftn/chiefcook/data/ChiefCookDBHelper.java
@@ -0,0 +1,59 @@
+package rs.ac.uns.ftn.chiefcook.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by daliborp on 3.9.16..
+ */
+public class ChiefCookDBHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "chiefcook.db";
+
+    public ChiefCookDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        addRecipeTable(db);
+        addIngredientTable(db);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    private void addRecipeTable(SQLiteDatabase db) {
+        final String SQL_CREATE_RECIPE_TABLE = "CREATE TABLE " + ChiefCookContract.RecipeEntry.TABLE_NAME + " (" +
+                ChiefCookContract.RecipeEntry._ID + " INTEGER PRIMARY KEY, " +
+                ChiefCookContract.RecipeEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
+                ChiefCookContract.RecipeEntry.COLUMN_READY_IN_MINUTES + " INTEGER NOT NULL, " +
+                ChiefCookContract.RecipeEntry.COLUMN_IMAGE_URL + " TEXT, " +
+                ChiefCookContract.RecipeEntry.COLUMN_API_ID + " INTEGER NOT NULL " +
+                " );";
+
+        db.execSQL(SQL_CREATE_RECIPE_TABLE);
+    }
+
+    private void addIngredientTable(SQLiteDatabase db) {
+        final String SQL_CREATE_INGREDIENT_TABLE = "CREATE TABLE " + ChiefCookContract.IngredientEntry.TABLE_NAME + " (" +
+                ChiefCookContract.IngredientEntry._ID + " INTEGER PRIMARY KEY, " +
+                ChiefCookContract.IngredientEntry.COLUMN_NAME + " TEXT NOT NULL, " +
+                ChiefCookContract.IngredientEntry.COLUMN_AMOUNT + " INTEGER NOT NULL, " +
+                ChiefCookContract.IngredientEntry.COLUMN_UNIT + " TEXT, " +
+                ChiefCookContract.IngredientEntry.COLUMN_UNIT_SHORT + " TEXT, " +
+                ChiefCookContract.IngredientEntry.COLUMN_IMAGE_URL + " TEXT, " +
+                ChiefCookContract.IngredientEntry.COLUMN_ORIGINAL_STRING + " TEXT, " +
+                ChiefCookContract.IngredientEntry.COLUMN_API_ID + " INTEGER NOT NULL, " +
+                ChiefCookContract.IngredientEntry.COLUMN_RECIPE_ID + " INTEGER NOT NULL, " +
+                "FOREIGN KEY (" + ChiefCookContract.IngredientEntry.COLUMN_RECIPE_ID + ") " +
+                "REFERENCES " + ChiefCookContract.RecipeEntry.TABLE_NAME + " (" + ChiefCookContract.RecipeEntry._ID + ")" +
+                " );";
+
+        db.execSQL(SQL_CREATE_INGREDIENT_TABLE);
+    }
+}
