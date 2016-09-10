5b96f400e0a5446c2b66ce4628f30e966a0f07b4
Dalibor Pavicic <dalibor-pavicic@hotmail.com>
2016-09-10 12:27:22 +0200

    Replace custom ingredients dialog with android alert dialog for consistent layout

--- app/src/main/java/rs/ac/uns/ftn/chiefcook/ui/fragments/IngredientsDialogFragment.java
+++ app/src/main/java/rs/ac/uns/ftn/chiefcook/ui/fragments/IngredientsDialogFragment.java
@@ -1,18 +1,19 @@
 package rs.ac.uns.ftn.chiefcook.ui.fragments;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import rs.ac.uns.ftn.chiefcook.R;
 import rs.ac.uns.ftn.chiefcook.model.ExtendedIngredient;
@@ -23,11 +24,8 @@ import rs.ac.uns.ftn.chiefcook.ui.adapters.IngredientsAdapter;
  */
 public class IngredientsDialogFragment extends DialogFragment {
 
-    public static final String KEY_INGREDIENTS = "ingredients";
     public static final String LOG_TAG = IngredientsDialogFragment.class.getSimpleName();
-
-    @BindView(R.id.lvIngredients) protected ListView lvIngredients;
-    @BindView(R.id.btnAddToShoppingList) protected Button btnAddToShoppingList;
+    public static final String KEY_INGREDIENTS = "ingredients";
 
     private IngredientsAdapter ingredientsAdapter;
     private ArrayList<ExtendedIngredient> ingredients;
@@ -54,38 +52,47 @@ public class IngredientsDialogFragment extends DialogFragment {
         ingredients = getArguments().getParcelableArrayList(KEY_INGREDIENTS);
     }
 
-    @Nullable
+    @NonNull
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_ingredients, container);
-        ButterKnife.bind(this, rootView);
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        btnAddToShoppingList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                List<ExtendedIngredient> selectedIngredients = ingredientsAdapter.getSelectedIngredients();
-                Listener listener = (Listener) getActivity();
-                listener.onAddToShoppingList(selectedIngredients);
-                dismiss();
-            }
-        });
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.fragment_ingredients_dialog, null);
 
-        return rootView;
+        ButterKnife.bind(this, dialogView);
+
+        ingredientsAdapter = createIngredientsAdapter(ingredients);
+
+        AlertDialog ingredientsDialog = builder.setView(dialogView)
+                .setTitle(R.string.ingredients_dialog_title)
+                .setPositiveButton(R.string.dialog_shopping_list, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        List<ExtendedIngredient> selectedIngredients = ingredientsAdapter.getSelectedIngredients();
+                        Listener listener = (Listener) getActivity();
+
+                        Log.d(LOG_TAG, String.format("Number of selected ingredients: %d", selectedIngredients.size()));
+                        listener.onAddToShoppingList(selectedIngredients);
+                        dismiss();
+                    }
+                })
+                .setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dismiss();
+                    }
+                })
+                .setAdapter(ingredientsAdapter, null)
+                .create();
+
+        return ingredientsDialog;
     }
 
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        getDialog().setTitle(
-                getContext().getResources().getString(R.string.ingredients_dialog_title));
-
+    private IngredientsAdapter createIngredientsAdapter(ArrayList<ExtendedIngredient> ingredients) {
         ExtendedIngredient[] ingredientsArray = new ExtendedIngredient[ingredients.size()];
         ingredients.toArray(ingredientsArray);
 
-        ingredientsAdapter = new IngredientsAdapter(getActivity(), ingredientsArray);
-        lvIngredients.setAdapter(ingredientsAdapter);
+        return new IngredientsAdapter(getActivity(), ingredientsArray);
     }
-
 }
